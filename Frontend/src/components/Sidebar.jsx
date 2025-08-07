2559938ff6c861b426012fbd4fda967f14ffff60
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-08-07 17:51:58 +0530

    sidebar

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const Sidebar = (selectedUser , setselectedUser) => {
   return (
     <div>
       
+      <div className="pb-5 ">
+        
+        <div className="flex justify-between items-center">
+          <img src={assets.logo}alt="logo" className='max-w-40' />
+          <div className="relative py-2 group">
+           <img src={assets.menu_icon} alt='' className='max-h-5 cursor-pointer'/> 
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
