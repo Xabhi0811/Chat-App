19838430d0b36c533025c282ea57999dc756f844
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-08-13 23:50:42 +0530

    frontend

--- Frontend/context/AuthContext.jsx
+++ Frontend/context/AuthContext.jsx
@@ -1,21 +1,109 @@
- import { Children, createContext, useState } from "react";
+import { Children, createContext, useEffect, useState } from "react";
 import axios from 'axios'
+import toast from "react-hot-toast";
+import {io} from "socket.io-client"
 
-  const backendUrl = import.meta.env. VITE_BACKEND_URL
-  axios.defaults. backendUrl =  backendUrl
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = backendUrl
  
- export const AuthContext = createContext();
+export const AuthContext = createContext();
 
- export const AuthProvider =({ Children}) =>{
-    const [token , setToken] = useState(localStorage.getItem("token"))
-    const [authUser , setAuthUser] = useState(null)
-     const value ={
+export const AuthProvider = ({ Children }) => {
+    const [token, setToken] = useState(localStorage.getItem("token"))
+    const [authUser, setAuthUser] = useState(null)
+    const [onlineUsers, setOnlineUsers] = useState([])
+    const [socket, setSocket] = useState(null)
 
-     }
- }
+    // check user is Authorized if so, set the user data and connect 
+    const checkAuth = async () => {
+        try {
+            const {data} = await axios.get("/api/auth/check")
+            if(data.success) {
+                setAuthUser(data.user)
+                connectSocket(data.user)
+            }
+            checkAuth()
+        } catch(error) {
+            toast.error(error.message)
+        }
+    }
 
- return (
-    <AuthContext.Provider value={value}>
-        {Children}
-    </AuthContext.Provider>
- )
+    // login function to handle user authentication and socket connection
+    const login = async (state, credentials) => {
+        try {
+            const {data} = await axios.post(`/api/auth/${state}`, credentials)
+            if(data.success) {
+                setAuthUser(data.userData)
+                connectSocket(data.userData)
+                axios.defaults.headers.common["token"] = data.token
+                setToken(data.token)
+                localStorage.setItem("token", data.token)
+                toast.success(data.message)
+            } else {
+                toast.error(data.message)
+            }
+        } catch(error) {
+            toast.error(error.message)
+        }
+    }
+
+    // logout function to handle user logout and socket disconnection
+    const logout = async () => {
+        localStorage.removeItem("token")
+        setToken(null)
+        setAuthUser(null)
+        setOnlineUsers([])
+        axios.defaults.headers.common["token"] = null
+        toast.success("Logged out successfully")
+        socket?.disconnect()
+    }
+
+    // user can update profile
+    const updateProfile = async (body) => {
+        try {
+            const {data} = await axios.put("/api/auth/update-profile", body)
+            setAuthUser(data.user)
+            toast.success("Profile updated successfully")
+        } catch(error) {
+            toast.error(error.message)
+        }
+    }
+
+    // socket function to handle socket
+    const connectSocket = (userData) => {
+        if(!userData || socket?.connected) return
+        const newSocket = io(backendUrl, {
+            query: {
+                userId: userData._id,
+            }
+        })
+        newSocket.connect()
+        setSocket(newSocket)
+
+        newSocket.on("getOnlineUsers", (userIds) => {
+            setOnlineUsers(userIds)
+        })
+    }
+
+    useEffect(() => {
+        if(token) {
+            axios.defaults.headers.common["token"] = token
+        }
+    }, [token])
+
+    const value = {
+        axios,
+        authUser, 
+        onlineUsers,
+        socket, 
+        login,
+        logout,
+        updateProfile
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {Children}
+        </AuthContext.Provider>  
+    )
+}
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePages from './pages/HomePage'
 import LoginPages from './pages/LoginPages'
 import ProfilePages from './pages/ProfilePages'
+import { Toaster } from "react-hot-toast"
+import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
+  const context = useContext(AuthContext)
+  const authUser = context?.authUser // Optional chaining to prevent errors
+
   return (
-    < div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster/>
       <Routes>
-        <Route path='/' element={<HomePages/>}/>
-         <Route path='/login' element={<LoginPages/>}/>
-          <Route path='/profile' element={<ProfilePages/>}/>
+        <Route path='/' element={authUser ? <HomePages/> : <Navigate to='/login'/>}/>
+        <Route path='/login' element={!authUser ? <LoginPages/> : <Navigate to='/'/>}/>
+        <Route path='/profile' element={authUser ? <ProfilePages/> : <Navigate to='/login'/>}/>
       </Routes>
     </div>
   )
