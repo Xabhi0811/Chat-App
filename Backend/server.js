843eb62f99732dd91a1738ece9966ea3a626820c
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-08-16 01:48:42 +0530

    for vercel changes

--- Backend/server.js
+++ Backend/server.js
@@ -47,5 +47,12 @@ app.use('/api/status', (req, res) => res.send("Server is live"));
 app.use("/api/auth",router);
 app.use("/api/message",rashi);
 
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+}
+
+// for vercel
+export default app;
+
